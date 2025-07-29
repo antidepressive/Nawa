d457cabf5621ee0c0c36a2bb23a5258aab0e8f0c
NawwafAlHamdan <165419897+NawwafAlHamdan@users.noreply.github.com>
2025-07-30 01:54:44 +0300

    Update switch.tsx

--- client/src/components/ui/switch.tsx
+++ client/src/components/ui/switch.tsx
@@ -1,26 +1,27 @@
 import * as React from "react"
-import * as SliderPrimitive from "@radix-ui/react-slider"
+import * as SwitchPrimitives from "@radix-ui/react-switch"
 
 import { cn } from "../../lib/utils"
 
-const Slider = React.forwardRef<
-  React.ElementRef<typeof SliderPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
+const Switch = React.forwardRef<
+  React.ElementRef<typeof SwitchPrimitives.Root>,
+  React.ComponentPropsWithoutRef<typeof SwitchPrimitives.Root>
 >(({ className, ...props }, ref) => (
-  <SliderPrimitive.Root
-    ref={ref}
+  <SwitchPrimitives.Root
     className={cn(
-      "relative flex w-full touch-none select-none items-center",
+      "peer inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input",
       className
     )}
     {...props}
+    ref={ref}
   >
-    <SliderPrimitive.Track className="relative h-2 w-full grow overflow-hidden rounded-full bg-secondary">
-      <SliderPrimitive.Range className="absolute h-full bg-primary" />
-    </SliderPrimitive.Track>
-    <SliderPrimitive.Thumb className="block h-5 w-5 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50" />
-  </SliderPrimitive.Root>
+    <SwitchPrimitives.Thumb
+      className={cn(
+        "pointer-events-none block h-5 w-5 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0"
+      )}
+    />
+  </SwitchPrimitives.Root>
 ))
-Slider.displayName = SliderPrimitive.Root.displayName
+Switch.displayName = SwitchPrimitives.Root.displayName
 
-export { Slider }
+export { Switch }
